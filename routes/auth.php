6519c215893703b50d213e1c6fcd6f3503ea31c5
Borhan Uddin <borhanidb@gmail.com>
2026-05-05 18:00:04 +0600

    add nixpacks config for node 22

--- routes/auth.php
+++ routes/auth.php
@@ -11,8 +11,7 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [AuthenticatedSessionController::class, 'create'])
-->name('login');
+Route::get('/', [AuthenticatedSessionController::class, 'create']);
 Route::get('login', [AuthenticatedSessionController::class, 'create'])
 ->name('login');
 Route::post('login', [AuthenticatedSessionController::class, 'store']);
